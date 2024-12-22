44dd5b633d0fd771b8d83a78497312619b7716a7
Gocha <development.11101110101@gmail.com>
2024-12-22 16:30:31 +0100

    Update README and index.html for clarity; add async function in main.js for weather data fetching

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pet-adoption-center
 Fake Adoption Center - Find Your New Bestie!
+
+<img src="./images/page-screen.jepg" />
--- main.js
+++ main.js
@@ -8,3 +8,14 @@ window.onload = function () {
     window.scrollTo(0, localStorage.getItem('scrollPosition'));
   }
 }
+
+// await keyword can only be used inside an async function, because it is part of the async/await feature in JavaScript and can't be used in the global scope.
+async function start() {
+
+  // Await means that the function will wait for the promise to resolve before continuing
+  // fetch() function returns a promise that resolves to the Response to that request, whether it is successful or not.
+  // The promise in javascript means that it hasn't happened yet, but it's promising that it will happen at some point in the future. That's why we have to await for it to actually trully finish.
+  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast");
+
+  const weatherData = await weatherPromise.json();
+}
